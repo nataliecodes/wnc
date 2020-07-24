55b9ef5753f074edfe89808e1b440b979eb96a46
nataliecodes <nataliepolen@gmail.com>
2020-07-24 15:01:58 -0400

    Add donate route.

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.post('/donate', (req, res) => res.send('donate route'));
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
